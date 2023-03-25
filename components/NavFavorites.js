c55ec3b1b5fe7ebddc816da5439474462b3bd4aa
NwaguChima <nwaguchima48@gmail.com>
2023-03-25 16:30:06 +0100

    rendered home icon screens

--- components/NavFavorites.js
+++ components/NavFavorites.js
@@ -5,7 +5,8 @@ import {
   FlatList,
   TouchableOpacity,
 } from 'react-native';
-import React from 'react';
+import { Icon } from 'react-native-elements';
+import tw from 'twrnc';
 
 const data = [
   {
@@ -27,9 +28,22 @@ const NavFavorites = () => {
     <FlatList
       data={data}
       keyExtractor={(item) => item.id}
-      renderItem={({ item }) => (
-        <TouchableOpacity>
-          <Text>{item.location}</Text>
+      ItemSeparatorComponent={() => (
+        <View style={[tw`bg-gray-200`, { height: 0.7 }]} />
+      )}
+      renderItem={({ item: { location, destination, icon } }) => (
+        <TouchableOpacity style={tw`flex-row items-center p-5`}>
+          <Icon
+            style={tw`mr-4 rounded-full bg-gray-300 p-3`}
+            name={icon}
+            type="ionicon"
+            color="white"
+            size={18}
+          />
+          <View>
+            <Text style={tw`font-semibold text-lg`}>{location}</Text>
+            <Text style={tw`text-gray-500`}>{destination}</Text>
+          </View>
         </TouchableOpacity>
       )}
     />
