48a7cca239bdae1c827a4d63716977ed44768b74
NwaguChima <nwaguchima48@gmail.com>
2023-03-25 14:36:30 +0100

    dispatched actions

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,8 +4,12 @@ import tw from 'twrnc';
 import NavOptions from '../components/NavOptions';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { GOOGLE_MAPS_APIKEY } from '@env';
+import { useDispatch } from 'react-redux';
+import { setDestination, setOrigin } from '../slices/navSlice';
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+
   return (
     <SafeAreaView style={`bg-white h-full`}>
       <View style={tw`p-5`}>
@@ -27,8 +31,14 @@ const HomeScreen = () => {
           minLength={2}
           enablePoweredByContainer={false}
           onPress={(data, details = null) => {
-            console.log('data->', data);
-            console.log('details->', details);
+            dispatch(
+              setOrigin({
+                location: details.geometry.location,
+                description: data.description,
+              })
+            );
+
+            dispatch(setDestination(null));
           }}
           fetchDetails={true}
           returnKeyType={'search'}
