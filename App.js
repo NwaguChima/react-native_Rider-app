29cfc213e0304dd2f19e09b48a37c094e29d66fd
NwaguChima <nwaguchima48@gmail.com>
2023-03-25 09:02:17 +0100

    exported selectors and store

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
+import { store } from './store';
 
 export default function App() {
   return (
--- slices/navSlice.js
+++ slices/navSlice.js
@@ -22,6 +22,14 @@ export const navSlice = createSlice({
   },
 });
 
+// Actions
 export const { setOrigin, setDestination, setTravelTimeInformation } =
   navSlice.actions;
+
+//   Selectors
+export const selectOrigin = (state) => state.nav.origin;
+export const selectDestination = (state) => state.nav.destination;
+export const selectTravelTimeInformation = (state) =>
+  state.nav.travelTimeInformation;
+
 export default navSlice.reducer;
