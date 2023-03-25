51d5e0dac6fcd64ba50419cd687176bbbf89db11
NwaguChima <nwaguchima48@gmail.com>
2023-03-25 16:17:40 +0100

    added zoom padding

--- components/Map.js
+++ components/Map.js
@@ -17,7 +17,9 @@ const Map = () => {
     if (!origin || !destination) return;
 
     // Zoom and fit to markers
-    mapRef.current.fitToSuppliedMarkers(['origin', 'destination'], {});
+    mapRef.current.fitToSuppliedMarkers(['origin', 'destination'], {
+      edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
+    });
   }, [origin, destination]);
 
   return (
@@ -51,6 +53,18 @@ const Map = () => {
           title="Origin"
           description={origin.description}
           identifier="origin"
+        />
+      )}
+
+      {destination?.location && (
+        <Marker
+          coordinate={{
+            latitude: destination.location.lat,
+            longitude: destination.location.lng,
+          }}
+          title="Destination"
+          description={destination.description}
+          identifier="destination"
         />
       )}
     </MapView>
