d47364d0740c48d949f5d9faa1aab2302c93869a
NwaguChima <nwaguchima48@gmail.com>
2023-03-25 09:26:03 +0100

    added logo

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import tw from 'twrnc';
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView>
-      <Text style={styles.container}>I am the HomeScreen</Text>
+    <SafeAreaView style={`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image
+          style={{
+            width: 100,
+            height: 100,
+            resizeMode: 'contain',
+          }}
+          source={{
+            uri: 'https://links.papareact.com/gzs',
+          }}
+        />
+      </View>
     </SafeAreaView>
   );
 };
