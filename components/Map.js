af70b53aa4cc7744be8916e894f3317657a4a304
NwaguChima <nwaguchima48@gmail.com>
2023-03-25 15:04:38 +0100

    updated redux slice bug

--- components/Map.js
+++ components/Map.js
@@ -1,11 +1,24 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import MapView from 'react-native-maps';
+import { useSelector } from 'react-redux';
+import { selectOrigin } from '../slices/navSlice';
+import tw from 'twrnc';
 
 const Map = () => {
+  const origin = useSelector(selectOrigin);
+
   return (
-    <View>
-      <Text>Map</Text>
-    </View>
+    <MapView
+      style={tw`flex-1`}
+      mapType="mutedStandard"
+      initialRegion={{
+        latitude: origin?.location.lat,
+        longitude: origin?.location.lng,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+    />
   );
 };
 
